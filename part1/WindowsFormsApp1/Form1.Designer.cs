93d23e92747d3423deafa1cb0f321f012740e507
miqueasarriondo <miqueasarriondo@gmai.com>
2025-06-12 17:41:49 -0300

    .

---
+++ part1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsolicitar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbMostrarValor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnsolicitar
+            // 
+            this.btnsolicitar.Location = new System.Drawing.Point(73, 12);
+            this.btnsolicitar.Name = "btnsolicitar";
+            this.btnsolicitar.Size = new System.Drawing.Size(100, 23);
+            this.btnsolicitar.TabIndex = 1;
+            this.btnsolicitar.Text = "solicitar valor";
+            this.btnsolicitar.UseVisualStyleBackColor = true;
+            this.btnsolicitar.Click += new System.EventHandler(this.btnsolicitar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "resultados";
+            // 
+            // lbMostrarValor
+            // 
+            this.lbMostrarValor.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbMostrarValor.Location = new System.Drawing.Point(73, 66);
+            this.lbMostrarValor.Name = "lbMostrarValor";
+            this.lbMostrarValor.Size = new System.Drawing.Size(100, 20);
+            this.lbMostrarValor.TabIndex = 3;
+            this.lbMostrarValor.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 129);
+            this.Controls.Add(this.lbMostrarValor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnsolicitar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "ejemplo modal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnsolicitar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lbMostrarValor;
+    }
+}
+
---
+++ part1/WindowsFormsApp1/Form3.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btncancelar = new System.Windows.Forms.Button();
+            this.btnaceptar = new System.Windows.Forms.Button();
+            this.tbValor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "valor";
+            // 
+            // btncancelar
+            // 
+            this.btncancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btncancelar.Location = new System.Drawing.Point(19, 46);
+            this.btncancelar.Name = "btncancelar";
+            this.btncancelar.Size = new System.Drawing.Size(75, 23);
+            this.btncancelar.TabIndex = 1;
+            this.btncancelar.Text = "cancelar";
+            this.btncancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnaceptar
+            // 
+            this.btnaceptar.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnaceptar.Location = new System.Drawing.Point(100, 46);
+            this.btnaceptar.Name = "btnaceptar";
+            this.btnaceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnaceptar.TabIndex = 2;
+            this.btnaceptar.Text = "aceptar";
+            this.btnaceptar.UseVisualStyleBackColor = true;
+            // 
+            // tbValor
+            // 
+            this.tbValor.Location = new System.Drawing.Point(53, 20);
+            this.tbValor.Name = "tbValor";
+            this.tbValor.Size = new System.Drawing.Size(100, 20);
+            this.tbValor.TabIndex = 3;
+            this.tbValor.TextChanged += new System.EventHandler(this.tbValor_TextChanged);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(218, 113);
+            this.Controls.Add(this.tbValor);
+            this.Controls.Add(this.btnaceptar);
+            this.Controls.Add(this.btncancelar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btncancelar;
+        private System.Windows.Forms.Button btnaceptar;
+        public System.Windows.Forms.TextBox tbValor;
+    }
+}
